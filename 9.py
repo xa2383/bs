35ccd332dbe2888b8b9ea8818b8367ea17e0c9f7
xa2383 <167450938+xa2383@users.noreply.github.com>
2024-07-05 09:13:57 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,12 +1,12 @@
 <?php
-print "<h3>REFRESH PAGE</h3>";
-$name= "counter.txt";
-$file=fopen($name,"r");
-$hits=fscanf($file,"%d");
+print "<h3> REFRESH PAGE </h3>";
+$name="counter.txt";
+$file = fopen($name,"r");
+$hits= fscanf($file,"%d");
 fclose($file);
 $hits[0]++;
-$file=fopen($name,"w");
+$file = fopen($name,"w");
 fprintf($file,"%d",$hits[0]);
 fclose($file);
-print "Total no of views:" .$hits[0];
+print "Total number of views: ".$hits[0];
 ?>
